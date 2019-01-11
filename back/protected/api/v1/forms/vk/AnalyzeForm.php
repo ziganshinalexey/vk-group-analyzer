d1cbde20d1199cbf397d1aa5881294ec9bc2475b
Alexey Ziganshin <ziga.alexey@gmail.com>
2019-01-11 17:37:04 +0700

    #

--- back/protected/api/v1/forms/vk/AnalyzeForm.php
+++ back/protected/api/v1/forms/vk/AnalyzeForm.php
@@ -11,17 +11,67 @@ use yii\base\Model;
  */
 class AnalyzeForm extends Model
 {
+    /**
+     * Свойство содержит урл пользователя в вк.
+     *
+     * @var string|null
+     */
+    protected $vkUrl;
+
+    /**
+     * Метод возвращает урл пользователя в вк.
+     *
+     * @return string|null
+     */
+    public function getVkUrl(): ?string
+    {
+        return $this->vkUrl;
+    }
+
+    /**
+     * Переопределенный метод возвращает правила валидации формы.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            [
+                'vkUrl',
+                'required',
+            ],
+            [
+                'vkUrl',
+                'string',
+                'max' => 255,
+            ],
+            [
+                'vkUrl',
+                'customVkUrlValidate',
+            ],
+        ];
+    }
+
+    /**
+     * Кастомный метод валидации урла пользователя.
+     *
+     * @param string $attribute Название атрибута.
+     *
+     * @return void
+     */
+    public function customVkUrlValidate(string $attribute): void
+    {
+        if (false === preg_match('/^https:\/\/vk.com\/[a-z0-9_]{5,32}$/i', $this->$attribute)) {
+            $this->addError($attribute, 'Ссылка неверно указана.');
+        }
+    }
 
     /**
      * Осуществлет основное действие формы - просмотр элемента.
      *
-     * @param array $params Параметры формы для выполнения её действия.
-     *
-     * @inherit
-     *
      * @return null
      */
-    public function run(array $params = [])
+    public function run()
     {
         if (! $this->validate()) {
             return null;
